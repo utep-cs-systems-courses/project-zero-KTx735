277c72fad0c61bc03d1c9f09cbca4e43e160851d
KTx735 <mygame1980@hotmail.com>
2021-09-11 20:05:41 -0600

    Added print arrow method and use of new fonts when printing char

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Char in new font = e or f, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -28,6 +28,16 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case 'e':
+      puts("You selected chars:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
+      break;
+    case 'f':
+      puts("You selected chars:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_5x7(c);
       break;
     case 'a':
       puts("You selected arrow:");
